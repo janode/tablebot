7f886147cb6b2d820a948c74d0ad6233e538b48e
Jan Ødegård <jan.odegard@novanet.no>
2022-08-10 01:24:22 +0200

    Change to main restaurant

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const baseUrl = `https://book.dinnerbooking.com/no/nb-NO`;
 export const numberOfPeople = 2;
-export const areaId = 4; // 1
+export const areaId = 1; // 1
 
 export const restaurants = [
   {
